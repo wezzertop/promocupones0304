ea5f774e4fcb1ca3a51b3603a1d2239a6ac9a6f5
Daniel <wezzertop@gmail.com>
2026-03-12 19:38:31 -0600

    Anuncion Movile

--- src/components/ProfileAd.tsx
+++ src/components/ProfileAd.tsx
@@ -50,14 +50,11 @@ export default function ProfileAd({ className, variant = 'sidebar' }: ProfileAdP
   }
 
   // Determine which config to use
-  let config = mobileConfig
-
-  if (!isMobile) {
-    if (variant === 'sidebar') {
-      config = desktopSidebarConfig
-    } else {
-      config = desktopFeedConfig
-    }
+  let config
+  if (variant === 'sidebar') {
+    config = desktopSidebarConfig
+  } else {
+    config = desktopFeedConfig
   }
 
   // Create the HTML content for the iframe
