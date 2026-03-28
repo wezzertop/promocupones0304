620f0f98c7bedb57643f92ab0d68b94f2cce47f3
Daniel <wezzertop@gmail.com>
2026-03-27 18:04:44 -0600

    Initial commit: Cupoferta Elite UI17

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,6 @@ const nextConfig: NextConfig = {
 
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000', '192.168.1.72:3000', '10.0.2.2:3000'],
       bodySizeLimit: '5mb',
     },
   },
